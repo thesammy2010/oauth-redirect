a20cfdb0947d58f2717094ecae1aa92beb2fc39c
Leo <you@example.com>
2020-12-23 04:06:36 +0000

    updated formatting for response messages and allowed for error messages

--- run.py
+++ run.py
@@ -12,18 +12,23 @@ api = Api(app)
 class AuthToken(Resource):
 
     def get(self):
-        embed: dict = {"title": "Retrieved redirect", "description": f"Data: {json.dumps(flask.request.args)}"}
-
-        data: dict = {"username": "oauth-redirects", "content": "", "embeds": [embed]}
-        requests.post(
-            url=os.getenv("webhook_url"), data=json.dumps(data), headers={"Content-Type": "application/json"}
+        embed = {"title": "Received redirect", "description": json.dumps(flask.request.args)}
+        data = {"username": "OAuth Redirect Response", "content": flask.request.args.get("appid"), "embeds": [embed]}
+        r = requests.post(
+            url=os.getenv("webhook_url"), data=json.dumps(data, indent=4), headers={"Content-Type": "application/json"}
         )
 
-        return flask.make_response(flask.jsonify(flask.request.args), 200)
+        if r.status_code == 200:
+            return flask.make_response(flask.jsonify({"Result": "Success! Result has been sent to Discord"}), 200)
+        else:
+            return flask.make_response(flask.jsonify(
+                    {"Result": "Failure! Result has failed to be sent to Discord. Check the logs"}
+                ), 400
+            )
 
 
 api.add_resource(AuthToken, "/")
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+    app.run(debug=False, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
